391e69cf3006a893f7f16ee3c99b0f333cd04552
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-26 14:13:30 -0700

    import App in todo_redux.jsx and replace h1 element in ReactDOM.render with App component

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { configureStore } from './store/store';
+import App from './components/app';
 
 let store = configureStore;
 window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<h1>Todos App</h1>, document.getElementById("root"));
+  ReactDOM.render(<App />, document.getElementById("root"));
 });
