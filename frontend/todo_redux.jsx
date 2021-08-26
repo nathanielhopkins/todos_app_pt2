8795a9d0d4c4eca37faab38107875d513d8d23b2
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-26 14:44:04 -0700

    remove App import and remove App from ReactDOM.render in todo_redux.jsx; instead import Root and pass Root component with store= store props to ReactDOM.render

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { configureStore } from './store/store';
-import App from './components/app';
+import Root from './components/root';
 
 let store = configureStore;
 window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(<Root store={store}/>, document.getElementById("root"));
 });
