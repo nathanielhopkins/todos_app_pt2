8bc1befaef756271c45c9acd56597b600f995fd6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-27 11:45:34 -0700

    render TodoForm in TodoList

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import TodoListItem from '../todo_list/todo_list_item';
+import TodoForm from "../todo_list/todo_form";
 
 export default class TodoList extends React.Component {
   render() {
     return(
-      <ul>
+      <div>
+      <TodoForm />
+      <ul className='todo-list'>
         {this.props.todos.map(todo => <TodoListItem todo={todo} />)}
       </ul>
+      </div>
     )
   }
 }
