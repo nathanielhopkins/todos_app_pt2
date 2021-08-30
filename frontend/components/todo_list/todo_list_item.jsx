52284a2b61020ae137a80d80eb9ed7c91cc4bdc8
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-30 14:04:46 -0700

    add button to TodoListItem to handle "Done" status; write conditional case to switch value of button from Done to Undo

--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -12,6 +12,7 @@ export default class TodoListItem extends React.Component {
     return(
       <li className='todo-list-item'>
         <h3>{todo.title}</h3>
+        <button>{todo.done == false ? 'Done' : 'Undo'}</button>
         <button onClick={(e)=>this.handleDelete(e, todo)}>Delete</button>
       </li>
     )
