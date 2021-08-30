ffe6f5f6494b63f0a4df10c1d62cea5789937152
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-30 10:44:53 -0700

    destructure props todos and receiveTodo in TodoList; refactor the map function that creates TodoListItems, using destructured variable; receiveTodo now passes the correct prop from container to TodoForm

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import TodoListItem from '../todo_list/todo_list_item';
 import TodoForm from "../todo_list/todo_form";
-import { receiveTodo } from "../../actions/todo_actions";
+
 
 export default class TodoList extends React.Component {
   render() {
+    const { todos, receiveTodo } = this.props;
     return(
       <div>
         <TodoForm receiveTodo={receiveTodo} />
       <ul className='todo-list'>
-        {this.props.todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+        {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
       </ul>
       </div>
     )
