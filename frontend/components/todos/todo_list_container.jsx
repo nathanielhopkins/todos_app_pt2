4df46bfc8c4dd8873d35dfc85644d59ca8cc89f8
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-30 10:47:26 -0700

    import receiveTodo from todo_actions.js in todo_list_container.jsx; modify mapStateToProps to return state as well

--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -1,9 +1,11 @@
 import { connect } from "react-redux";
 import TodoList from './todo_list';
 import allTodos from "../../reducers/selectors";
+import { receiveTodo } from "../../actions/todo_actions";
 
 const mapStateToProps = state => ({
-  todos: allTodos(state)
+  todos: allTodos(state),
+  state
 });
 
 const mapDispatchToProps = dispatch => ({
