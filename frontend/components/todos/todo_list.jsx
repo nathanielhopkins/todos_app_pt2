2704d74ad8f4726ee2205be819649cc2c9986d4f
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-30 11:06:16 -0700

    pass receiveTodo and removeTodo to TodoListItem within TodoList render

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -3,14 +3,23 @@ import TodoListItem from '../todo_list/todo_list_item';
 import TodoForm from "../todo_list/todo_form";
 
 
+
 export default class TodoList extends React.Component {
   render() {
-    const { todos, receiveTodo } = this.props;
+    const { todos, receiveTodo, removeTodo } = this.props;
     return(
       <div>
         <TodoForm receiveTodo={receiveTodo} />
       <ul className='todo-list'>
-        {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+        {todos.map(todo => 
+          (<TodoListItem 
+            key={todo.id} 
+            todo={todo} 
+            receiveTodo={receiveTodo}
+            removeTodo={removeTodo}
+            />
+            
+            ))}
       </ul>
       </div>
     )
