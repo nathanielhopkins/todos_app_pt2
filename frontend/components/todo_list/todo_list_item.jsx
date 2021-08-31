a58b06d8ec6b5a537f4ae0cf26aeb7bde9f26254
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-30 17:23:28 -0700

    initialize state.detail as false in TodoListItem; write toggleView that switches state.detail boolean and set it to the onClick for the todo.title in render; have TodoList render a TodoDetailView only if state.detail is true

--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import TodoDetailView from './todo_detail_view';
 
 export default class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { detail: false };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleDone = this.handleDone.bind(this);
+    this.toggleView = this.toggleView.bind(this);
+  }
+
   handleDelete(e) {
     e.preventDefault();
     this.props.removeTodo(this.props.todo);
@@ -14,14 +24,22 @@ export default class TodoListItem extends React.Component {
     this.props.receiveTodo(toggled);
   }
   
+  toggleView(e) {
+    e.preventDefault();
+    this.setState({detail: !this.state.detail });
+  }
+
   render() {
     const { todo } = this.props;
-
+    let detail;
+    if(this.state.detail) {
+      detail = <TodoDetailView todo={todo} />;
+    }
     return(
       <li className='todo-list-item'>
-        <h3 onClick={(e)=>console.log(this.props.todo)}>{todo.title}</h3>
+        <h3><a onClick={this.toggleView}>{todo.title}</a></h3>
         <button onClick={(e)=>this.handleDone(e)}>{todo.done == false ? 'Done' : 'Undo'}</button>
-        <button onClick={(e) =>this.handleDelete(e)}>Delete</button>
+        {detail}
       </li>
     )
   };
