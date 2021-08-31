7b51c5b194c7f398886120edc728db0faa4346f4
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-31 11:21:25 -0700

    modify todo_list_container.jsx to destructor the import of allTodos

--- frontend/components/todo_list/todo_list_container.jsx
+++ frontend/components/todo_list/todo_list_container.jsx
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import TodoList from '../todo_list/todo_list';
-import allTodos from "../../reducers/selectors";
+import { allTodos } from "../../reducers/selectors";
 import { receiveTodo, removeTodo } from "../../actions/todo_actions";
 
 const mapStateToProps = state => ({
