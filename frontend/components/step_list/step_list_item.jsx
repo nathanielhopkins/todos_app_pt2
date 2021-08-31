5b5aeb63980d468757d8d30076e80e5f326871f5
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-31 15:56:36 -0700

    write out StepListItem; initialize state.done as false; write toggleDone that flips this.state.done boolean; write out render that displays steps title, body, and buttons for toggleDone and removeStep

--- frontend/components/step_list/step_list_item.jsx
+++ frontend/components/step_list/step_list_item.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { stepsByTodoId } from '../../reducers/selectors';
 
 export default class StepListItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { done: false}
+  }
+  
+  toggleDone(e) {
+    e.preventDefault();
+    this.setState({done: !this.state.done})
+  }
+
   render() {
-    <p>StepListItem</p>
+    const { title, body } = this.props.step;
+    const { removeStep, receiveStep } = this.props;
+
+    return(
+    <div>
+      <h4 className='step-list-item-title'>{title}</h4>
+      <p className='step-list-item-body'>{body}</p>
+      <button 
+        className='step-list-item-toggle'
+        onClick={this.toggleDone}
+        >{this.state.done ? "Undo" : "Done"}</button>
+      <button 
+        className='step-list-item-delete'
+        onClick={removeStep}
+        >Delete Step</button>
+    </div>
+    );
   }
 }
