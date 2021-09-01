4a2c85f2210d073d9e27d8d7fab967a12fed1bd7
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-01 15:36:36 -0700

    remove seeded initialState values from todos_reducer.js

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,19 +1,6 @@
 import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from '../actions/todo_actions';
 
-const initialState = {
-  1: {
-    id: 1,
-    title: "wash car",
-    body: "with soap",
-    done: false
-  },
-  2: {
-    id: 2,
-    title: "wash dog",
-    body: "with shampoo",
-    done: true
-  }
-};
+const initialState = {};
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
