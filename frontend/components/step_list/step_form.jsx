d633e69e6c82a4626853f86d421b6fd64fc53d84
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-01 10:00:46 -0700

    set values of text input to equal state values; write updateValue function, bind to this in constructor and assign as onChange event for input text fields

--- frontend/components/step_list/step_form.jsx
+++ frontend/components/step_list/step_form.jsx
@@ -7,18 +7,32 @@ export default class StepForm extends React.Component {
     this.state = {
       title: '',
       body: '',
+      todoId: this.props.todoId,
       done: false
     };
+
+    this.updateValue = this.updateValue.bind(this);
   };
+
+  updateValue(e, key) {
+    this.setState({key: e.currentTarget.value});
+  }
 
   render() {
     return(
       <div className='step-form'>
         <label>Title: 
-          <input type='text' placeholder='step1'></input>
+          <input type='text' 
+            placeholder='step1' 
+            value={this.state.title}
+            onChange={(e) => this.updateValue(e, 'title')}>
+            </input>
         </label>
         <label>Body: 
-          <input type='text' placeholder='do the first thing'></input>
+          <input type='text' 
+            placeholder='do the first thing'
+            value={this.state.body}
+            onChange={(e) => this.updateValue(e, 'body')}></input>
         </label>
         <button >Toggle</button>
         <button>Delete Step</button>
