9ee49d417ca59d24114bb946453a90e053c86a6f
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-01 12:05:02 -0700

    import StepListItemContainer in step_list.jsx; pass step.id to each li in StepList as unique key; pass receiveStep to StepForm

--- frontend/components/step_list/step_list.jsx
+++ frontend/components/step_list/step_list.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import StepForm from './step_form';
+import StepsListItemContainer from './step_list_item_container';
 
 export default class StepList extends React.Component { 
   render() {
-    const { steps, todoId } = this.props;
+    const { steps, todoId, receiveStep } = this.props;
     
     return(
       <div>
         <ul>
-          {steps.map(step => <li><StepsListItemContainer step={step} /></li>)}
+          {steps.map(step => <li key={step.id}><StepsListItemContainer step={step} /></li>)}
         </ul>
-        <StepForm todoId={todoId} />
+        <StepForm receiveStep={receiveStep} todoId={todoId} />
       </div>
     );
   };
